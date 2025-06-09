832a6d035ba7875813316a478e1712c6047c9e6c
CarolSof <164248360+CarolSof@users.noreply.github.com>
2025-06-09 12:20:18 -0500

    Proyecto organizado

---
+++ storage/framework/views/5a53d3f23259d908a47c9a9f436cc16c.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH C:\Users\User\Downloads\PROJECTOV2\PROJECTbaji\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
